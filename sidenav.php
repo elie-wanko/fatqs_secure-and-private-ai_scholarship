031572eba1f78458bc2705374f611fcddb58c616
shashi.gharti@gmail.com <shashi.gharti@gmail.com>
2019-06-12 15:55:59 +0545

    removed hardcoded drive link

--- sidenav.php
+++ sidenav.php
@@ -3,7 +3,7 @@ require 'google-clients/drive-client.php';
 // Get the API client and construct the service object.
 $client = getDriveClient();
 $httpClient = $client->authorize();
-$response = $httpClient->get("https://www.googleapis.com/drive/v2/files?q='1a2V6obuD_A-A2PEo3Pk6-_uG1jxVcMTR'+in+parents&key=LhdVdfYOjlrVYJ8f4ZuBJlJF");
+$response = $httpClient->get(getenv("google_drive"));
 
 $lessons = json_decode($response->getBody()->getContents())->items;
 ?>
