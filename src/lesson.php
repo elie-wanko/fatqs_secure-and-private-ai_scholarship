c8d4b111ce3215b26e44161a66bf7c3b237a13d5
shashi.gharti@gmail.com <shashi.gharti@gmail.com>
2019-06-24 09:07:52 +0545

    Question display bug solved

--- src/lesson.php
+++ src/lesson.php
@@ -54,7 +54,7 @@ if (empty($answerValues)) {
                     foreach ($questions as $key => $question) {
 
                         echo "<li>";
-                        echo '<a href="#" class="question" data-index="'. $key.'" data-question="' . $question . '" >' . $question . '</a>';
+                        echo '<a href="#" class="question" data-index="'. $key.'" >' . $question . '</a>';
                         echo "</li>";
                     }
                     ?>
@@ -67,7 +67,7 @@ if (empty($answerValues)) {
                     $answer = isset($answers[$key]) ? $answers[$key] : '';
                 ?>
                     <div class="answers--content" data-index="<?php echo $key ?>">
-                        <p><b>Q.:  </b><strong class="question-title text-accent-1"></strong></p>
+                        <p><b>Q.:  </b><strong class="question-title text-accent-1"><?php echo $question ?></strong></p>
                         <p>
                             <strong><b>A.:  </b></strong>
                             <span class="answer"><?php echo $answer ?></span>
